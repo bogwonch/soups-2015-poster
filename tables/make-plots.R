cab0e11fa07c8d1779e5f27a9552fc95ff85e63f
Joseph Hallett <bogwonch@bogwonch.net>
2015-05-20 12:04:07 +0100

    next draft

--- tables/make-plots.R
+++ tables/make-plots.R
@@ -8,18 +8,33 @@ malware      <- read.csv('malware.csv')
 pus          <- read.csv('pus.csv')
 unconcerned  <- read.csv('unconcerned.csv')
 
+installs <- read.csv('user-installs.csv')
+
 setEPS()
-postscript("lin.eps")
+postscript("lin.eps", width=6, height=4)
+
 lin <- merge(merge(merge(conservative, advanced), fencesitter), unconcerned)
-d_lin <- melt(lin, id.vars=c(1))
-p_lin <- ggplot(d_lin, aes(x=value, fill=variable)) + geom_histogram(binwidth=0.03) + scale_y_sqrt() 
+lin_ <- merge(lin, installs)
+lin__ <- lin_[lin_$installs > 20,]
+lin___ <- lin__[,1:5]
+
+d_lin <- melt(lin___, id.vars=c(1))
+p_lin <- ggplot(d_lin, aes(x=value, fill=variable)) + geom_histogram()
 update_labels(p_lin, list(x="Percentage of users apps meeting policy", y="User count", color="Policy"))
 dev.off()
 
 setEPS()
-postscript("malware.eps")
-bad <- merge(pus,malware)
-d_bad <- melt(bad, id.vars=c(1))
-p_bad <- ggplot(d_bad, aes(x=value, fill=variable)) + geom_histogram(binwidth=0.03) + scale_y_sqrt()
+postscript("malware.eps", width=6, height=3)
+pus_ <- pus[pus$pus<1.0,]
+malware_ <- malware[malware$malware<1.00,]
+bad <- merge(pus_,malware_)
+
+bad_ <- merge(bad, installs)
+bad__ <- bad_[bad_$installs > 20,]
+bad___ <- bad__[,1:3]
+
+d_bad <- melt(bad___, id.vars=c(1))
+p_bad <- ggplot(d_bad, aes(x=value, fill=variable)) + geom_histogram(binwidth=0.02) # + scale_y_sqrt()
 update_labels(p_bad, list(x="Percentage of users apps meeting policy", y="User count", color="Policy"))
 dev.off()
+
